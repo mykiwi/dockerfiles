79c3248f1847417303f8d9e0b5ea4e999ee58015
Romain Gautier <mail@romain.sh>
2017-10-08 10:59:15 +0200

    improve symfony-base fpm images

--- symfony-base/5.6-fpm/docker/php/bin/entrypoint.sh
+++ symfony-base/5.6-fpm/docker/php/bin/entrypoint.sh
@@ -3,9 +3,8 @@
 user=$(stat -c %u /srv)
 group=$(stat -c %g /srv)
 
-groupadd -g $group group
-useradd --shell /bin/bash -u $user -g $group -o -c "" -m user
-export HOME=/home/user
+groupadd -g $group group > /dev/null 2>&1 || true
+useradd --shell /bin/bash --no-create-home --home-dir /home -u $user -g $group -o -c "" user > /dev/null 2>&1 || true
 
 sed -i "s/user = www-data/user = $user/g" /usr/local/etc/php-fpm.d/www.conf
 sed -i "s/group = www-data/group = $group/g" /usr/local/etc/php-fpm.d/www.conf
